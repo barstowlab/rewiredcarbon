a190fa9ae48c754616c13c5a5472e465205bc4e5
Buz Barstow <buzb@me.com>
2019-11-13 10:23:34 -0500

    Updated sfig-transmembrane

--- sfig-transmembrane.py
+++ sfig-transmembrane.py
@@ -1,3 +1,14 @@
+#!/opt/local/bin/python3.7
+
+# ------------------------------------------------------------------------------------------------ #
+# sfig-transmembrane.py
+# Calculates rewired carbon fixation efficiency as a function of transmembrane voltage
+# 
+# Farshid Salimijazi and Buz Barstow
+# Last updated by Buz Barstow on 2019-11-13
+# ------------------------------------------------------------------------------------------------ #
+
+
 from rewiredcarbon.scenario import ImportScenarioTable, CalculateScenarioEfficiencies, \
 Plot_Efficiency_Scattergraphs, Export_Efficiency_Scattergraphs
 
@@ -6,25 +17,19 @@ from rewiredcarbon.utils import ensure_dir
 import os
 
 
-scenarioTableFileName = 'input/Scenario Table/Scenario Table Transmembrane Voltage.csv'
+scenarioTableFileName = 'input/sfig-transmembrane.csv'
+outputFileDirname = 'output/sfig-transmembrane/'
+
+ensure_dir(outputFileDirname)
+
+outputFilePrefix = 'transmembrane'
+
 
 scenarioDict = ImportScenarioTable(scenarioTableFileName)
 
 efficienciesDict = CalculateScenarioEfficiencies(scenarioDict, mode='scattergraph')
 
 Plot_Efficiency_Scattergraphs(efficienciesDict, 'effTotalElectricalToFuel')
-
-inputFileBasename = os.path.basename(scenarioTableFileName)
-outputFileDirname = os.path.dirname(scenarioTableFileName)
-[outputFilePrefix, inputFileExtension] = os.path.splitext(inputFileBasename)
-
-
-outputFileDirname = \
-'output/Supplementary Figure 1 {sfig-transmembrane} - Efficiency vs Transmembrane Voltage'
-
-ensure_dir(outputFileDirname + '/')
-
-outputFilePrefix = 'transmembrane'
 
 Export_Efficiency_Scattergraphs(outputFileDirname, outputFilePrefix, efficienciesDict, \
 'effTotalElectricalToFuel', keysToPlot=None)
